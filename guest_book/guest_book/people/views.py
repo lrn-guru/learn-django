b2c45d6058450fbbb994e35a1e97c90781029710
Razzi <razzi53@gmail.com>
2014-02-15 18:24:31 -0500

    clean getting-started

--- guest_book/guest_book/people/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from guest_book.people.models import Person
-
-def home(request):
-    people = Person.objects.all()
-    summary = ''
-    for person in people:
-        summary += person.name + ' says ' + person.message + '.<br>'
-    return HttpResponse(summary)
