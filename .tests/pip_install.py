d082ac25c62b5562da1e25f0408b968289c0f1d4
Razzi <razzi53@gmail.com>
2014-02-16 05:36:08 -0500

    'squelch dj-admin'

--- .tests/pip_install.py
+++ .tests/pip_install.py
@@ -1,7 +1,7 @@
-from subprocess import check_output, CalledProcessError
+from subprocess import check_output, CalledProcessError, STDOUT
 
 try:
-	check_output('django-admin.py --version'.split())
+	check_output('django-admin.py --version'.split(), stdout=open('/dev/null', 'w', sterr=STDOUT))
 	exit(0)
 except CalledProcessError:
 	exit(1)
---
+++ .tests/startproject.py
@@ -0,0 +1,7 @@
+from subprocess import check_output
+
+files = check_output('find .')
+if 'manage.py' in files:
+    exit(0)
+else:
+    exit(1)
